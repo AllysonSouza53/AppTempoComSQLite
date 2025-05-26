e1b78285cb57d65d51851b476a5e6d8f6bede955
allyson_souza <souzaallyson53@gmail.com>
2025-05-26 14:45:33 -0300

    2° entrada, finalização do Back

--- AppTempoComSQLite/Services/database.cs
+++ AppTempoComSQLite/Services/database.cs
@@ -5,8 +5,9 @@ namespace MauiAppTempoSQLite.Services
 {
     public class DataService
     {
-        public static async Task<Tempo?> GetPrevisao(string cidade)
+        public static async Task<Tempo?> PegarPrevisao(string cidade)//recebe cidade para achar previsão
         {
+            //acesso a API
             Tempo? t = null;
 
             string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
@@ -14,37 +15,37 @@ namespace MauiAppTempoSQLite.Services
             string url = $"https://api.openweathermap.org/data/2.5/weather?" +
                          $"q={cidade}&units=metric&appid={chave}";
 
-            using (HttpClient client = new HttpClient())
+            using (HttpClient cliente = new HttpClient())//cria instancia HTTP
             {
-                HttpResponseMessage resp = await client.GetAsync(url);
+                HttpResponseMessage resp = await cliente.GetAsync(url);
 
-                if (resp.IsSuccessStatusCode)
+                if (resp.IsSuccessStatusCode)//verifica se foi bem sucedido
                 {
                     string json = await resp.Content.ReadAsStringAsync();
 
                     var rascunho = JObject.Parse(json);
 
-                    DateTime time = new();
-                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
-                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+                    DateTime temp = new();
+                    DateTime NascSol = temp.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime PorSol = temp.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
 
-                    t = new()
+                    t = new()//cria o objeto tempo
                     {
                         lat = (double)rascunho["coord"]["lat"],
                         lon = (double)rascunho["coord"]["lon"],
-                        Descricao = (string)rascunho["weather"][0]["description"],
+                        Descricao = (string)rascunho["weather"][0]["Descricao"],
                         main = (string)rascunho["weather"][0]["main"],
-                        TempMin = (double)rascunho["main"]["temp_min"],
-                        TempMax = (double)rascunho["main"]["temp_max"],
-                        velocidade = (double)rascunho["wind"]["speed"],
-                        visibilidade = (int)rascunho["visibility"],
-                        NascSol = sunrise.ToString(),
-                        PorSol = sunset.ToString(),
-                    }; // Fecha obj do Tempo.
-                } // Fecha if se o status do servidor foi de sucesso
-            } // fecha laço using
+                        TempMin = (double)rascunho["main"]["TempMin"],
+                        TempMax = (double)rascunho["main"]["TempMax"],
+                        velocidade = (double)rascunho["wind"]["Velocidade"],
+                        visibilidade = (int)rascunho["Visibilidade"],
+                        NascSol = NascSol.ToString(),
+                        PorSol = PorSol.ToString(),
+                    };
+                }
+            }
 
-            return t;
+            return t;//retorna objeto
         }
     }
 }
