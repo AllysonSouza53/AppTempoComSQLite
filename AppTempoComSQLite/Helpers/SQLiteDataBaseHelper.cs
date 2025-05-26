2c5b341069b52c67eeaf65f35645b23809777f3a
allyson_souza <souzaallyson53@gmail.com>
2025-05-26 10:53:18 -0300

    10:52 - 26/05. Inicio da Programação do App, Api e BackEnd

--- AppTempoComSQLite/Helpers/SQLiteDataBaseHelper.cs
+++ AppTempoComSQLite/Helpers/SQLiteDataBaseHelper.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppTempoSQLite.Models;
+using SQLite;
 
-namespace AppTempoComSQLite.Helpers
+namespace MauiAppTempoSQLite.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;//Cria a conexão assincrona e o _conn mantem ela aberta
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);//Cria a tabela Tempo
+            _conn.CreateTableAsync<Tempo>().Wait();//Força a conexão assincrona, garantindo que a tabela esteja pronta antes de seguir
+        }
+
+        public Task<int> Inserir(Tempo p)//insere valores
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<int> Deletar(int id)//deleta valores
+        {
+            return _conn.Table<Tempo>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Tempo>> PesquiserTudo()//busca todos os dados da tabela e os retorna
+        {
+            return _conn.Table<Tempo>().ToListAsync();
+        }
+
+        public Task<List<Tempo>> Pesquisar(string q)// pesquisa dados especificos da tabela e os retorna
+        {
+            string sql = "SELECT * FROM Tempo " +
+                         "WHERE description LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Tempo>(sql);
+        }
     }
 }
--- AppTempoComSQLite/Models/tempo.cs
+++ AppTempoComSQLite/Models/tempo.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
-namespace AppTempoComSQLite.Models
+namespace MauiAppTempoSQLite.Models
 {
-    internal class tempo
+    public class Tempo
+        //declara os rotulos que serão registrado no banco
     {
+        [PrimaryKey, AutoIncrement]//define o ID como chave primaria e Auto Increment
+        public int Id { get; set; }
+        public double? lon { get; set; }
+        public double? lat { get; set; }
+        public double? TempMin { get; set; }
+        public double? TempMax { get; set; }
+        public int? visibilidade { get; set; }
+        public double? velocidade { get; set; }
+        public string? main { get; set; }
+        public string? Descricao { get; set; }
+        public string? NascSol { get; set; }
+        public string? PorSol { get; set; }
     }
 }
