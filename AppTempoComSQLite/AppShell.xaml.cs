e562b0d2e1bdf93da3d4342f31077df1ddcbcd51
allyson_souza <souzaallyson53@gmail.com>
2025-05-26 10:24:54 -0300

    Adicionar arquivos de projeto.

---
+++ AppTempoComSQLite/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTempoComSQLite
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppTempoComSQLite/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppTempoComSQLite
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
